69b7230ce7607c2af9dc9c72fd0e382a6f7dddc2
Ben Halverson <benhalverson33@gmail.com>
2018-11-03 20:35:06 -0700

    code

--- index.js
+++ index.js
@@ -8,24 +8,12 @@ const OAuthClient = require('intuit-oauth');
 const app = express();
 const router = express.Router();
 const port = process.env.PORT || 3000;
-const oauthClient = new OAuthClient({
-  clientId: process.env.clientId,
-  clientSecret: process.env.clientSecret,
-  environment: process.env.environment,
-  redirectUri: process.env.redirectUri
-});
 
 mongoose.Promise = global.Promise;
 mongoose
   .connect(db.url)
   .then(() => console.log(`Connected to Database`))
   .catch(err => console.error(`Failed to connect ${err}`));
-
-// AuthorizationUri
-const authUri = oauthClient.authorizeUri({
-  scope: [OAuthClient.scopes.Accounting, OAuthClient.scopes.OpenId],
-  state: 'testState'
-}); // can be an array of multiple scopes ex : {scope:[OAuthClient.scopes.Accounting,OAuthClient.scopes.OpenId]}
 
 app.use(morgan('dev'));
 app.use(cors());
@@ -38,9 +26,29 @@ router.get('/', (req, res) => {
   res.redirect(authUri);
 });
 
-router.get('/about', function(req, res) {
+router.get('/about', (req, res) => {
   res.json({ message: 'New api endpoint works' });
 });
+
+router.get('/payment', (req, res) => {
+  const oauthClient = new OAuthClient({
+    clientId: process.env.clientId,
+    clientSecret: process.env.clientSecret,
+    environment: process.env.environment,
+    redirectUri: process.env.redirectUri
+  });
+  // AuthorizationUri
+  const authUri = oauthClient.authorizeUri({
+    scope: [OAuthClient.scopes.Accounting, OAuthClient.scopes.OpenId],
+    state: 'testState'
+  }); // can be an array of multiple scopes ex : {scope:[OAuthClient.scopes.Accounting,OAuthClient.scopes.OpenId]}
+
+  res.redirect(authUri);
+});
+router.post('/create-event', (req, res) => {});
+router.get('/events', (req, res) => {});
+
+router.get('/events/:id', (req, res) => {});
 
 app.use('/api', router);
 
